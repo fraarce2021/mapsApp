85955aeab97a0d22613415c50206fa8b541f822d
Francisco <danielarcesoftware@gmail.com>
2021-08-28 00:15:39 -0700

    Zoom in and zoom out applied in zone range component, I'm working in markers component

--- src/app/maps/pages/markers/markers.component.ts
+++ src/app/maps/pages/markers/markers.component.ts
@@ -1,16 +1,125 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
+
+interface MarkerColor {
+  color:string;
+  marker?:mapboxgl.Marker;
+  center?:[number,number];
+}
 
 @Component({
   selector: 'app-markers',
   templateUrl: './markers.component.html',
   styles: [
+    `
+      .map-container {
+        width: 100%;
+        height: 100%;
+      }
+
+      .list-group{
+        position:fixed;
+        top:20px;
+        right:20px;
+        z-index:99;
+      }
+      li{
+        cursor:pointer;
+      }
+    `
   ]
 })
-export class MarkersComponent implements OnInit {
+export class MarkersComponent implements AfterViewInit {
+
+  @ViewChild('map') divMapa!: ElementRef;
+  map!: mapboxgl.Map;
+  zoomLevel:number = 15;
+  center:[number,number] = [-84.2053, 10.026689];
+
+  //Marker Array
+  markers: MarkerColor[]=[];
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    this.map = new mapboxgl.Map({
+      container: this.divMapa.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: this.center,
+      zoom: this.zoomLevel,
+    });
+
+    this.readMarkersLocalStorage();
+    // const markerHtml: HTMLElement = document.createElement('div');
+    // markerHtml.innerHTML = 'Hello World!';
+    // new mapboxgl.Marker(
+    //   {
+    //     element: markerHtml
+    //   }
+    // )
+    // .setLngLat(this.center)
+    // .addTo(this.map)
   }
 
+  addMarker(){
+    const color = "#xxxxxx".replace(/x/g, y=>(Math.random()*16|0).toString(16));
+
+    const newMarker = new mapboxgl.Marker({
+      draggable:true,
+      color
+    })
+      .setLngLat(this.center)
+      .addTo(this.map);
+
+      this.markers.push({
+        color,
+        marker:newMarker
+      });
+      this.saveMarkersInLocalStorage();
+  }
+
+  goMarker(value:mapboxgl.Marker){
+    this.map.flyTo({
+      center:  value.getLngLat()
+    });
+  }
+
+  saveMarkersInLocalStorage(){
+    const lngLatArray: MarkerColor[] = [];
+
+    this.markers.forEach(m=>{
+      const color = m.color;
+      const {lng,lat} = m.marker!.getLngLat();
+
+      lngLatArray.push({
+        color,
+        center: [lng,lat]
+      });
+
+    });
+
+    localStorage.setItem('markers', JSON.stringify(lngLatArray));
+  }
+
+  readMarkersLocalStorage(){
+    if (!localStorage.getItem('markers')) {
+      return;
+    }
+    const lngLatArray: MarkerColor[] = JSON.parse(localStorage.getItem('markers')!);
+
+    lngLatArray.forEach(m=>{
+      const newMarker = new mapboxgl.Marker({
+        draggable:true,
+        color: m.color
+      })
+      .setLngLat(m.center!)
+      .addTo(this.map);
+
+      this.markers.push({
+        marker: newMarker,
+        color: m.color
+      })
+    });
+
+  }
 }
--- src/app/maps/pages/zoom-range/zoom-range.component.ts
+++ src/app/maps/pages/zoom-range/zoom-range.component.ts
@@ -1,16 +1,82 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, ViewChild } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-zoom-range',
   templateUrl: './zoom-range.component.html',
   styles: [
-  ]
+    `
+      .map-container {
+        width: 100%;
+        height: 100%;
+      }
+
+      .row {
+        width:400px;
+        background-color: white;
+        border-radius: 5px;
+        bottom: 50px;
+        left: 50px;
+        padding: 10px;
+        position: fixed;
+        z-index: 999;
+      }
+    `,
+  ],
 })
-export class ZoomRangeComponent implements OnInit {
+export class ZoomRangeComponent implements AfterViewInit, OnDestroy {
 
-  constructor() { }
+  @ViewChild('map') divMapa!: ElementRef;
+  map!: mapboxgl.Map;
+  zoomLevel:number = 16;
+  center:[number,number] = [-84.2053, 10.026689];
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngOnDestroy(){
+    this.map.off('zoom', ()=>{});
+    this.map.off('zoomend', ()=>{});
+    this.map.off('move', ()=>{});
   }
 
+  ngAfterViewInit(){
+    this.map = new mapboxgl.Map({
+      container: this.divMapa.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: this.center,
+      zoom: this.zoomLevel,
+    });
+
+    this.map.on('zoom', ()=>{
+      this.zoomLevel = this.map.getZoom();
+    });
+
+    this.map.on('zoomend', ()=>{
+
+      if (this.map.getZoom() > 18) {
+        this.map.zoomTo(18);
+      }
+
+    });
+
+    this.map.on('move', (event) =>{
+      const target = event.target;
+      const {lng, lat} = target.getCenter();
+      this.center = [lng,lat];
+    })
+  }
+
+  zoomOut() {
+    this.map.zoomIn();
+    this.zoomLevel = this.map.getZoom();
+  }
+
+  zoomIn() {
+    this.zoomLevel = this.map.getZoom();
+    this.map.zoomOut();
+  }
+
+  zoomChange(value: string){
+    this.map.zoomTo(Number(value))
+  }
 }
